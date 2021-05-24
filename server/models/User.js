1886478702d8a80680c1308a8160a2e6c337ae2d
ysemenyuk <invent@list.ru>
2021-05-24 03:54:14 +0300

    step1

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-ignore
 
 import { Model } from 'objection';
 import objectionUnique from 'objection-unique';
@@ -15,9 +15,11 @@ export default class User extends unique(Model) {
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['email', 'password'],
+      required: ['firstName', 'lastName', 'email', 'password'],
       properties: {
         id: { type: 'integer' },
+        firstName: { type: 'string', minLength: 1, maxLength: 255 },
+        lastName: { type: 'string', minLength: 1, maxLength: 255 },
         email: { type: 'string', format: 'email' },
         password: { type: 'string', minLength: 3 },
       },
@@ -30,5 +32,9 @@ export default class User extends unique(Model) {
 
   verifyPassword(password) {
     return encrypt(password) === this.passwordDigest;
+  }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -16,11 +16,17 @@ export default (app) => {
     .post('/users', async (req, reply) => {
       try {
         const user = await app.objection.models.user.fromJson(req.body.data);
-        await app.objection.models.user.query().insert(user);
+        console.log(1, user);
+
+        const dbUser = await app.objection.models.user.query().insert(user);
+        console.log(2, dbUser);
+
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
         return reply;
-      } catch ({ data }) {
+      } catch (err) {
+        const { data } = err;
+        console.log(3, err);
         req.flash('error', i18next.t('flash.users.create.error'));
         reply.render('users/new', { user: req.body.data, errors: data });
         return reply;
