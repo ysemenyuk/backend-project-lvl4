e6d0aa25fe66e270d5b1de8055d8430014881196
ysemenyuk <invent@list.ru>
2021-07-27 01:21:12 +0300

    linter

--- server/migrations/20210607235447_create_statuses_table.js
+++ server/migrations/20210607235447_create_statuses_table.js
@@ -1,10 +1,9 @@
-exports.up = (knex) =>
-  knex.schema.createTable('statuses', (table) => {
-    table.increments('id').primary();
-    table.string('name');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-    // table.foreign('task_id').references('id').inTable('tasks');
-  });
+exports.up = (knex) => knex.schema.createTable('statuses', (table) => {
+  table.increments('id').primary();
+  table.string('name');
+  table.timestamp('created_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
+  // table.foreign('task_id').references('id').inTable('tasks');
+});
 
 exports.down = (knex) => knex.schema.dropTable('statuses');
--- server/migrations/20210720233353_create_labels_table.js
+++ server/migrations/20210720233353_create_labels_table.js
@@ -1,9 +1,8 @@
-exports.up = (knex) =>
-  knex.schema.createTable('labels', (table) => {
-    table.increments('id').primary();
-    table.string('name');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  });
+exports.up = (knex) => knex.schema.createTable('labels', (table) => {
+  table.increments('id').primary();
+  table.string('name');
+  table.timestamp('created_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
+});
 
 exports.down = (knex) => knex.schema.dropTable('labels');
