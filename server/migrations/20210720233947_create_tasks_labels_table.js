596b851f520fa455897c0841df68de244b867fdd
ysemenyuk <invent@list.ru>
2021-07-30 01:28:26 +0300

    migrations

--- server/migrations/20210720233947_create_tasks_labels_table.js
+++ server/migrations/20210720233947_create_tasks_labels_table.js
@@ -1,8 +1,6 @@
 exports.up = (knex) => knex.schema.createTable('tasks_labels', (table) => {
   table.integer('task_id');
   table.integer('label_id');
-  table.foreign('task_id').references('id').inTable('tasks');
-  table.foreign('label_id').references('id').inTable('labels');
 });
 
 exports.down = (knex) => knex.schema.dropTable('tasks_labels');
